8cf6e4927a3118f39890f1eeb1bb3b203af65449
sherry <liqilibo@hotmail.com>
2021-02-08 16:04:35 +0800

    ci: update action to decide public path in CNAME and none-CNAME env

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,6 @@
-let publicPath = '/';
-
-if (process.env.NODE_ENV === 'production' && process.env.GITHUB_REPOSITORY) {
-  const getRepository = process.env.GITHUB_REPOSITORY.split('/')[1];
-  publicPath = `/${getRepository}/`;
-}
-
 module.exports = {
   lintOnSave: true,
   outputDir: 'dist',
-  publicPath,
+  publicPath: !!process.env.BASE ? process.env.BASE : '/',
   productionSourceMap: false,
 };
